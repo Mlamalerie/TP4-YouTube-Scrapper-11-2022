eedf1cdfe5be10c67223c02a2b6ec30556cdcf6a
Mlamali SAID SALIMO <saidsalimo@eisti.eu>
2022-11-17 02:54:38 +0100

    Finish TP

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import concurrent.futures
+
 import pandas as pd
 import numpy as np
 import requests
+
+from datetime import datetime
 from selenium import webdriver
 from selenium.common import StaleElementReferenceException, TimeoutException
 from selenium.webdriver.common.by import By
@@ -13,12 +17,10 @@ from selenium.webdriver.common.action_chains import ActionChains
 import json
 from bs4 import BeautifulSoup
 from time import sleep
-from typing import List,Tuple
-# %%
-URLS = ["https://www.youtube.com/watch?v=Wu919BSzEbY", "https://www.youtube.com/watch?v=nU5MSi5goW0",
-        "https://www.youtube.com/watch?v=8fqcivpFBio&t=568s", "https://www.youtube.com/watch?v=2ge0oJxlU68"]
+from typing import List, Tuple
+import json
 
-
+WORKERS = 3
 class YouTubeVideoScrapper():
 
     def __init__(self) -> None:
@@ -26,46 +28,55 @@ class YouTubeVideoScrapper():
         op = Options()
         op.headless = False
         self.driver = webdriver.Chrome(options=op)
-        self.driver.set_window_size(800,1100)
+        self.driver.set_window_size(800, 1100)
         self.driver.implicitly_wait(10)
 
-        self.wait = WebDriverWait(self.driver,20)
+        self.wait = WebDriverWait(self.driver, 20)
 
-    def go(self, ytb_video_url : str) -> None:
+        self.cookies_accepted = False
 
-        self.ytb_video_url = ytb_video_url
+    def go(self, video_id : str) -> None:
+
         # OPEN PAGE
-        self.driver.get(self.ytb_video_url)
+        self.driver.get(f"https://www.youtube.com/watch?v={video_id}")
+        self.video_id = video_id
+        self.current_url = self.driver.current_url
+        print(f"> {self.video_id}")
+
         # CLICK COOKIES
-        self.__click_accept_cookies()
+        if not self.cookies_accepted:
+            self.__click_accept_cookies()
         sleep(5)
         # CLICK DESCRIPTION
         self.__click_show_more_description()
-        sleep(1)
+        sleep(0.2)
         # SCROLL DOWN
-        self.__scroll_down()
+        self.__scroll_down(scroll_pause_time=1.5)
         sleep(1)
-        self.soup = BeautifulSoup(str(self.driver.page_source), 'lxml')
+
+        self.soup = BeautifulSoup(str((self.driver.page_source).encode('utf-8')), 'lxml')
 
 
-
-
-    def __click_accept_cookies(self)-> None:
-        button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="content"]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button')))
+    def __click_accept_cookies(self) -> None:
+        button = self.wait.until(EC.element_to_be_clickable(
+            (By.XPATH, '//*[@id="content"]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button')))
         button.click()
+        self.cookies_accepted = True
 
     def __click_show_more_description(self) -> None:
 
-        #button = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="expand"]')))
-        button = self.driver.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[3]/div[1]/div/ytd-text-inline-expander/tp-yt-paper-button[1]')
+        # button = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="expand"]')))
+        button = self.driver.find_element(By.XPATH,
+                                          '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[3]/div[1]/div/ytd-text-inline-expander/tp-yt-paper-button[1]')
         button.click()
 
-    def __scroll_down(self,scroll_pause_time = 1,k_scrolls=3):
+    def __scroll_down(self, scroll_pause_time=1, k_scrolls=6) -> None:
 
         # Get scroll height
+        print("... let's scrolling... ",end="")
         last_height = self.driver.execute_script("return document.documentElement.scrollHeight")
         k = 0
-        while k < k_scrolls:
+        while True:
             # Scroll down to bottom
             self.driver.execute_script("window.scrollTo(0, arguments[0]);", last_height)
             # Wait to load page
@@ -76,98 +87,108 @@ class YouTubeVideoScrapper():
             if new_height == last_height:
                 break
             last_height = new_height
-            k+=1
-        print("k",k)
-
-    def get_title(self,how_many=3) -> str:
-        print(how_many)
-        if how_many > 0:
-            try:
-                h1 = self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="title"]/h1')))
-            except StaleElementReferenceException:
-                return self.get_title(how_many-1)
-            else:
-                return h1.text
-        else:
-            return None
-
-    def get_author(self) -> Tuple[str,str]:
-        a = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#text > a")))
-        return a.text, a.get_attribute('href')
-
-    def get_likes(self) -> str:
-        span = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#segmented-like-button > ytd-toggle-button-renderer > yt-button-shape > button > div.cbox.yt-spec-button-shape-next--button-text-content > span")))
-        #span = self.driver.find_element(By.CSS_SELECTOR, "#segmented-like-button > ytd-toggle-button-renderer > yt-button-shape > button > div.cbox.yt-spec-button-shape-next--button-text-content > span")
-
-        return span.text
-
-
-    def get_description(self) -> str:
-
-        sleep(10)
-        pass
-
-    def get_links_from_description(self)-> List[str]:
-        pass
-
-    def get_id_video(self)-> str:
-        pass
-
-    def get_comments(self)-> List[dict]:
-        pass
-        # GET COMMENTS
-
-
+            k += 1
+        print(f"ok i'am at the bottom (after {k} scrolling) !")
 
     def get_all_infos(self) -> dict:
-        print("get_all_infos")
-        #page_source = self.driver.page_source.encode('utf-8').strip()
 
+        result = {"video_id" : self.video_id, "video_url": self.current_url}
 
-        div = self.soup.find("div", {"id":"above-the-fold"})
+        div_above_the_fold = self.soup.find("div", {"id": "above-the-fold"})
+        div_comments = self.soup.find("ytd-comments", {"id": "comments"})
 
-        title_item = div.find("h1",{'class' : 'style-scope ytd-watch-metadata'})
-        print(title_item.text)
-        author_item = div.find("a",{'class' : 'yt-simple-endpoint style-scope yt-formatted-string'})
-        print(author_item.text,f'https://www.youtube.com/{author_item["href"]}')
+        # TITLE
+        title_item = div_above_the_fold.find("h1", {'class': 'style-scope ytd-watch-metadata'})
+        result["video_title"] = title_item.text
 
-        #result['description'] = self.get_description()
-        #print(result)
+        # AUTHOR
+        author_item = div_above_the_fold.find("a", {'class': 'yt-simple-endpoint style-scope yt-formatted-string'})
+        result["video_author_name"] = author_item.text
+        result["video_author_url"] = f'https://www.youtube.com/{author_item["href"]}'
 
+        # VIEWS & DATE
+        spans = div_above_the_fold.find("div", {'id': "info-container"}).find_all("span")
+        result["video_views"] = spans[0].text.split(" ")[0]
+        result["video_published_date"] = spans[-1].text
 
+        # DESCRIPTION
+        desc_item = div_above_the_fold.find("ytd-text-inline-expander", {"id": "description-inline-expander"})
+        result['video_description_text'] = desc_item.text
+
+        # LINKS DESCRIPTION
+        links = [{"text": link.text, "href": link["href"]} for link in desc_item.find_all("a")]
+        result['video_description_links'] = links
+
+        # ID VIDEO
+        #result['video_id'] = self.current_url.split("?v=")[-1]
+
+        # COMMENTS
+        nb_comments_item = div_comments.find("h2", {"id": "count"}).find("span")
+        result["video_nb_comments"] = nb_comments_item.text
+
+        div_all_comments = div_comments.find("div", {"id": "contents"})
+
+        comments_result = []
+        for comment_div in div_all_comments.find_all("ytd-comment-thread-renderer"):
+            content_item = comment_div.find("yt-formatted-string", {"id": "content-text"})
+            vote_count_item = comment_div.find("span", {"id": "vote-count-middle"})
+            a_item = comment_div.find("a", {"id": "author-text"})
+
+            comments_result.append(
+                {"author_text": a_item.text.strip(), "author_href": a_item["href"], "content_text": content_item.text,
+                 "vote_count": vote_count_item.text})
+
+        result["video_comments"] = comments_result
+        print(f"... done.")
         return result
 
-#%%
-scrapper = YouTubeVideoScrapper()
-scrapper.go(URLS[0])
-#%%
-video_infos = scrapper.get_all_infos()
-#%%
-def main()-> None:
+
+def read_input_json(json_file_path: str) -> List[str]:
+    with open(json_file_path, "r") as f:
+        data = json.load(f)
+    return data["videos_id"][:2]
+
+def write_output_json(data,output_file_path="output.json"):
+    date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+    output_dict = {"date_time" :date_time, "data": data}
+    with open(output_file_path, "w") as f:
+        json.dump(output_dict, f,indent=4)
+    print(f"*******\njSON OUTPUT FILE : {output_file_path}\n*******")
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def start_scraping(videos_id):
+    scrapping_results = []
     scrapper = YouTubeVideoScrapper()
-    scrapper.go(URLS[0])
-    video_infos = scrapper.get_all_infos()
+    for video_id in videos_id:
+        scrapper.go(video_id=video_id)
+        scrapping_results.append(scrapper.get_all_infos())
+    return scrapping_results
+def main() -> None:
+    videos_id = read_input_json("input.json")
+    print(f"*******\nIDs TO SCRAP : {videos_id}\n*******")
+
+    p = len(videos_id)//WORKERS
+    p = p if len(videos_id)%WORKERS == 0 else p + 1
+    videos_id_chunks = list(chunks(videos_id, p))
+    print([len(v) for v in videos_id_chunks])
+
+    results = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        futures = []
+
+        futures.append(executor.map(start_scraping, videos_id_chunks))
+        for future in concurrent.futures.as_completed(results):
+            results += future.result()
 
 
-#%%
-r"""
-def main_0():
-    driver = webdriver.Chrome()
-    for url in URLS:
-        driver.get('{}/videos?view=0&sort=p&flow=grid'.format(url))
-        content = driver.page_source.encode('utf-8').strip()
-        soup = BeautifulSoup(content, 'lxml')
-        titles = soup.findAll('a', id='video-title')
-        views = soup.findAll('span', class_='style-scope ytd-grid-video-renderer')
-        video_urls = soup.findAll('a', id='video-title')
-        print('Channel: {}'.format(url))
-        i = 0  # views and time
-        j = 0  # urls
-        for title in titles[:10]:
-            print('\n{}\t{}\t{}\thttps://www.youtube.com{}'.format(title.text, views[i].text, views[i + 1].text,
-                                                                   video_urls[j].get('href')))
-            i += 2
-            j += 1
-"""
+    write_output_json(data=results)
+
 if __name__ == "__main__":
-    pass
+    main()
